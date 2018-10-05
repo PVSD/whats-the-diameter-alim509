ad3b2e8c236d71fc701e1853190cbe2de77fe07f
al5091 <angelovinolim@gmail.com>
2018-10-05 10:58:55 -0400

    Diameter3

---
+++ src/com/company/Circle.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Circle {
+
+    public double radius;
+    public Circle(double r) {
+
+        radius = r;
+
+    }
+
+    public double diameter() {
+
+        return 2 * radius;
+
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,8 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         Circle cir1 = new Circle(35.5);
-        double circleDiameter = cir1.diameter();
-        System.out.println(circleDiameter);
+        System.out.println(cir1.diameter());
 
     }
 }
